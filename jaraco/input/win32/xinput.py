0baafde3982b327e15ae960fc3123f67371e0a6b
jaraco <none@none>
2009-05-03 14:21:54 +0000

    Moved translation initialization into its own method

--- jaraco/input/win32/xinput.py
+++ jaraco/input/win32/xinput.py
@@ -111,15 +111,23 @@ class XInputJoystick(event.EventDispatcher):
 		self._last_state = self.get_state()
 		self.received_packets = 0
 		self.missed_packets = 0
-		
-		# Set the method that will be called to normalize
-		#  the values for analog axis.
+	
+		self.set_translate_method(normalize_axes)
+
+	def set_translate_method(self, normalize_axes):
+		"""
+		Set the method that will be called to normalize
+		the values for analog axis.
+		"""
 		choices = [self.translate_identity, self.translate_using_data_size]
 		self.translate = choices[normalize_axes]
 
 	def translate_using_data_size(self, value, data_size):
-		# normalizes analog data to [0,1] for unsigned data
-		#  and [-0.5,0.5] for signed data
+		"""
+		Normalizes analog data to [0,1] for unsigned data
+		and [-0.5,0.5] for signed data.
+		data_size is the binary size of the data in bytes.
+		"""
 		data_bits = 8*data_size
 		return float(value)/(2**data_bits-1)
 
