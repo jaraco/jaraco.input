9e4bb54a89b412cdc525df6c9f12305de53fb328
Jason R. Coombs <jaraco@jaraco.com>
2018-02-27 22:06:19 -0500

    Move binary logic into input package. Now there are some doctests to exercise and tests pass.

--- jaraco/input/win32/xinput.py
+++ jaraco/input/win32/xinput.py
@@ -15,12 +15,10 @@ import time
 from operator import itemgetter, attrgetter
 from itertools import count, starmap
 
-from six.moves import builtins
-
-import six
 from pyglet import event
 
-from jaraco.input.base import NormalizingAxisJoystick as NormalizingJS
+from ..base import NormalizingAxisJoystick as NormalizingJS
+from ..binary import get_bit_values
 
 
 class XINPUT_GAMEPAD(ctypes.Structure):
@@ -63,43 +61,6 @@ def struct_dict(struct):
 		field, type = pair
 		return field, getattr(struct, field)
 	return dict(map(get_pair, struct._fields_))
-
-
-def get_bit_values(number, size=32):
-	"""
-	Get bit values as a list for a given number
-
-	>>> get_bit_values(1) == [0]*31 + [1]
-	True
-
-	>>> get_bit_values(0xDEADBEEF)
-	[1, 1, 0, 1, 1, 1, 1, 0, 1, 0, 1, 0, ..., 0, 1, 1, 1, 1]
-
-	You may override the default word size of 32-bits to match your actual
-	application.
-	>>> get_bit_values(0x3, 2)
-	[1, 1]
-
-	>>> get_bit_values(0x3, 4)
-	[0, 0, 1, 1]
-	"""
-	res = list(gen_bit_values(number))
-	res.reverse()
-	# 0-pad the most significant bit
-	res = [0] * (size - len(res)) + res
-	return res
-
-
-def gen_bit_values(number):
-	"""
-	Return a zero or one for each bit of a numeric value up to the most
-	significant 1 bit, beginning with the least significant bit.
-	"""
-	if six.PY2:
-		number = builtins.long(number)
-	while number:
-		yield number & 0x1
-		number >>= 1
 
 
 ERROR_DEVICE_NOT_CONNECTED = 1167
