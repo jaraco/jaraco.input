ede5cc2841c0ec53cd89a1dab5b0176b0884f3ea
Jason R. Coombs <jaraco@jaraco.com>
2021-02-09 22:16:38 -0500

    🧎‍♀️ Genuflect to the types.

--- jaraco/input/__init__.py
+++ jaraco/input/__init__.py
@@ -10,6 +10,6 @@ import importlib
 platform_path = '.'.join((__name__, sys.platform))
 
 try:
-    Joystick = importlib.import_module(platform_path).Joystick
+    Joystick = importlib.import_module(platform_path).Joystick  # type: ignore
 except ImportError:
     pass
