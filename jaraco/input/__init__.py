0b4bac3bbd547ca4c4cafff17ecd9e62ab0e812d
Avasam <samuel.06@hotmail.com>
2024-08-25 17:38:04 -0400

    Pass mypy and link issues

--- jaraco/input/__init__.py
+++ jaraco/input/__init__.py
@@ -4,12 +4,12 @@ Input support
 
 __all__ = ['Joystick']
 
-import sys
 import importlib
+import sys
 
 platform_path = '.'.join((__name__, sys.platform))
 
 try:
-    Joystick = importlib.import_module(platform_path).Joystick  # type: ignore
+    Joystick = importlib.import_module(platform_path).Joystick
 except ImportError:
     pass
